e3b6fe83f52cbda41ffd5942c5f7fd25cf003598
vlemonade <125125657+vlemonade@users.noreply.github.com>
2023-11-27 15:01:05 +0800

    cssss

--- base/forms.py
+++ base/forms.py
@@ -7,20 +7,20 @@ class register_form(ModelForm):
         model = senior_list
         fields = '__all__'
         widgets = {
-            'first_name': forms.TextInput(attrs={'placeholder': 'First Name', 'required': True, 'class': 'form-control'}),
-            'last_name': forms.TextInput(attrs={'placeholder': 'Last Name', 'required': True, 'class': 'form-control'}),
-            'middle_name': forms.TextInput(attrs={'placeholder': 'Middle Name', 'required': True, 'class': 'form-control'}),
-            'suffix': forms.TextInput(attrs={'placeholder': 'Suffix', 'class': 'form-control'}),
-            'age': forms.NumberInput(attrs={'placeholder': 'Age', 'required': True, 'class': 'form-control'}),
+            'first_name': forms.TextInput(attrs={'placeholder': 'First Name', 'required': True}),
+            'last_name': forms.TextInput(attrs={'placeholder': 'Last Name', 'required': True}),
+            'middle_name': forms.TextInput(attrs={'placeholder': 'Middle Name', 'required': True}),
+            'suffix': forms.TextInput(attrs={'placeholder': 'Suffix'}),
+            'age': forms.NumberInput(attrs={'placeholder': 'Age', 'required': True}),
             #'sex': forms.TextInput(attrs={'placeholder': 'Sex', 'required': True, 'class': 'form-control form-select'}),
              'sex': forms.Select(choices=senior_list.SEX_CHOICES, attrs={'class': 'form-select'}),
             'birth_date': forms.DateInput(attrs={'required': True, 'class': 'form-control', 'type':'date'}),
-            'address': forms.TextInput(attrs={'placeholder': 'Address', 'required': True, 'class': 'form-control'}),
-            'OSCA_ID': forms.NumberInput(attrs={'placeholder': 'OSCA_ID', 'required': True, 'id': 'id_osca_id', 'class': 'form-control'}),
-            'senior_image': forms.FileInput(attrs={'required': False, 'id': 'image', 'class':'form-control'}), 
-            'phone_number': forms.TextInput(attrs={'placeholder': 'Contact No.', 'required': True, 'class': 'form-control'}),
+            'address': forms.TextInput(attrs={'placeholder': 'Address', 'required': True}),
+            'OSCA_ID': forms.NumberInput(attrs={'placeholder': 'OSCA_ID', 'required': True, 'id': 'id_osca_id'}),
+            'senior_image': forms.FileInput(attrs={'required': False, 'id': 'image'}), 
+            'phone_number': forms.TextInput(attrs={'placeholder': 'Contact No.', 'required': True}),
             'allowance_type': forms.Select(attrs={'class': 'form-control'}),  
-            'allowance_amount': forms.NumberInput(attrs={'placeholder': 'Allowance Amount', 'required': True, 'class': 'form-control'})
+            'allowance_amount': forms.NumberInput(attrs={'placeholder': 'Allowance Amount', 'required': True})
         }
 
     def clean_OSCA_ID(self):
